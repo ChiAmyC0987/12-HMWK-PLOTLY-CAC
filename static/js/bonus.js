19104728e86d50a23d0ee09580db22346ae1b77a
ChiAmyC <51067413+ChiAmyC0987@users.noreply.github.com>
2019-09-15 22:24:32 -0700

    homework level 1

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,1 +1,12 @@
+    //////////////////////// BONUS: Build the Gauge Chart/////////////////////////////
+    // buildGauge(data.WFREQ);
+    // Build a guage based on the sample (patient's)
+    // wash frequency (sample.WFREQ) in the metadata set
+    var data = [{domain: {x: [0, 1], y: [0, 1]}, value: sample.WFREQ , title: {text: "Weekly Cleaning"},
+    type: "indicator", mode: "gauge+number"}];
+
+    var layout = {width: 500, height: 500, margin: {t: 0, b: 0}};
+    Plotly.newPlot('gauge', data, layout);
+    ////////////////// @TODO: End Guage Chart using the sample data/////////////////
+
 
--- templates/app.js
+++ templates/app.js
@@ -37,24 +37,28 @@ function buildCharts(sample) {
     /////////////// @TODO: Build a Pie Chart//////////////////////////////////////
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-   
-
-
-
     var trace_pie = {
-    labels: ["1795", "922", "944", "2419", "1167", "2859", "2539", "2722", "482", "728"
-  ],
-    values: ["0.164847778162406", "0.0918068030681218", "0.053528157851586", "0.0391258403629275", "0.0386612494761965", "0.0365933645489825", "0.0352997977663199", "0.0274381911928143", "0.019676790496839", "0.018665622096307"
-  ],
-   type: "pie"
+    labels: ["1795", "922", "944", "2419", "1167", "2859", "2539", "2722", "482", "728"],
+    values: [16.48, 9.18, 5.35, 3.91, 3.86, 3.65, 3.52, 2.74, 1.96, 1.86],
+    growth: ["Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus", 
+    "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium", 
+    "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium", 
+    "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", 
+    "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", 
+    "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", 
+    "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", 
+    "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Finegoldia", 
+    "Bacteria", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales"],
+    type: 'pie'
      };
     var data = [trace_pie];
     var layout = {
-    title: "'Pie' Chart",
+    title: "OTU ID Chart",
     };
     Plotly.newPlot("plot", data, layout);
+  };
+  
 
-    /////////////// @TODO: Build a Bubble Chart using the sample data////////////
     var trace_bubble = {    
       x: xValues,
       y: yValues,
@@ -70,19 +74,6 @@ function buildCharts(sample) {
       xaxis: {title: "OTU ID"}
     };
     Plotly.newPlot('bubble', data, layout);  
-    //////////////////////// @TODO: End Bubble Chart using the sample data/////////
-    
-    //////////////////////// BONUS: Build the Gauge Chart/////////////////////////////
-    // buildGauge(data.WFREQ);
-    // Build a guage based on the sample (patient's)
-    // wash frequency (sample.WFREQ) in the metadata set
-    var data = [{domain: {x: [0, 1], y: [0, 1]}, value: sample.WFREQ , title: {text: "Weekly Cleaning"},
-    type: "indicator", mode: "gauge+number"}];
-
-    var layout = {width: 500, height: 500, margin: {t: 0, b: 0}};
-    Plotly.newPlot('gauge', data, layout);
-    ////////////////// @TODO: End Guage Chart using the sample data/////////////////
-
 }
 
 function init() {
@@ -112,4 +103,4 @@ function optionChanged(newSample) {
 }
 
 // Initialize the dashboard
-init();
+init()
